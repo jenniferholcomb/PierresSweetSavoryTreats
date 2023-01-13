6b6a92e19c2507d035bb29426c6dc19211792a89
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-01-13 10:37:11 -0800

    full CRUD for Treat

--- PierresBakery/Controllers/TreatsController.cs
+++ PierresBakery/Controllers/TreatsController.cs
@@ -24,13 +24,56 @@ namespace PierresBakery.Models
 
     public ActionResult Index()
     {
-      return View();
+      return View( _db.Treats.OrderBy(treat => treat.Name).ToList());
     }
 
     public ActionResult Create()
     {
   
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Treats.Update(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
